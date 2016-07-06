b658eacadee8336b75b87a87f764704ba698556b
rwoodall1 <randy@305spin.com>
2016-07-06 16:47:32 -0500

    Commit

--- BaseClass/Classes/SQLQuery.cs
+++ BaseClass/Classes/SQLQuery.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using NLog;
 namespace BaseClase.Classes {
     public class SQLQuery {
      
@@ -15,44 +16,45 @@ namespace BaseClase.Classes {
         public SQLQuery() {
             
             }
+        protected Logger Log { get; private set; }
+        public int ExecuteNonQueryAsync(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
+            int retval = 0;
 
-      //  public int ExecuteNonQueryAsync(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
-            //int sqlResult=0;
+            using (var connection = new SqlConnection(_ConnectionString))
+                {
+                using (var command = new SqlCommand(cmdText,connection))
+                    {
+                    try
+                        {
+                        command.CommandType = cmdType;
+                        command.Parameters.Clear();
+                        command.Parameters.AddRange(commandParameters);
+                        connection.Open();
 
-            //using (var connection = new SqlConnection(_ConnectionString))
-            //    {
-            //    using (var command = new SqlCommand(cmdText,connection))
-            //        {
-            //        try
-            //            {
-            //            command.CommandType = cmdType;
-            //            command.Parameters.Clear();
-            //            command.Parameters.AddRange(commandParameters);
-            //            connection.Open();
+                       var sqlResult = command.ExecuteNonQueryAsync();
+                        retval = sqlResult.Result;
+                         
+                        }
+                    catch (Exception ex)
+                        {
 
-            //             sqlResult = command.ExecuteNonQueryAsync();
-                        
-            //            }
-            //        catch (Exception ex)
-            //            {
-                       
-            //            Logger.Fatal("Error running ExecuteNonQueryAsync.",ex);
-                       
-            //            }
-            //        finally
-            //            {
-            //            connection.Close();
-            //            }
+                        Log.Fatal("Error running ExecuteNonQueryAsync.",ex.Message);
+                     
+                        }
+                    finally
+                        {
+                        connection.Close();
+                        }
 
-            //        return sqlResult;
-            //        }
-            //    }
+                    return retval;
+                    }
+                }
 
-          //  }
+            }
 
-        //public async Task<ServiceProcessingResult<object>> ExecuteReaderAsync<T>(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
+        //public async object ExecuteReaderAsync<T>(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
 
-        //    var processingResult = new ServiceProcessingResult<object> { IsSuccessful = true };
+        //    object retval;
 
         //    using (var connection = new SqlConnection(_ConnectionString))
         //        {
@@ -104,44 +106,43 @@ namespace BaseClase.Classes {
         //                }
         //            }
         //        }
-            //}
-
-        //public async Task<ServiceProcessingResult<DataTable>> ExecuteReaderAsync(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
-        //    var processingResult = new ServiceProcessingResult<DataTable> { IsSuccessful = true };
-
-        //    using (var connection = new SqlConnection(_ConnectionString))
-        //        {
-        //        using (var command = new SqlCommand(cmdText,connection))
-        //            {
-        //            try
-        //                {
-        //                var dt = new DataTable();
-        //                command.CommandType = cmdType;
-        //                command.Parameters.Clear();
-        //                command.Parameters.AddRange(commandParameters);
-        //                connection.Open();
-
-        //                using (SqlDataAdapter a = new SqlDataAdapter(command))
-        //                    {
-        //                    a.Fill(dt);
-        //                    }
-        //                processingResult.Data = dt;
-        //                return processingResult;
-        //                }
-        //            catch (Exception ex)
-        //                {
-        //                processingResult.IsSuccessful = false;
-        //                Logger.Fatal("Error running ExecuteReaderAsync.",ex);
-        //                processingResult.Error = new ProcessingError("Error running ExecuteReaderAsync. Ex: " + ex.ToString(),"Error running ExecuteReaderAsync. Ex: " + ex.ToString(),false,false);
-        //                return processingResult;
-        //                }
-        //            finally
-        //                {
-        //                connection.Close();
-        //                }
-        //            }
-        //        }
         //    }
+
+        public  DataTable ExecuteReaderAsync(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
+            DataTable dt=null;
+
+            using (var connection = new SqlConnection(_ConnectionString))
+                {
+                using (var command = new SqlCommand(cmdText,connection))
+                    {
+                    try
+                        {
+                        var dt = new DataTable();
+                        command.CommandType = cmdType;
+                        command.Parameters.Clear();
+                        command.Parameters.AddRange(commandParameters);
+                        connection.Open();
+
+                        using (SqlDataAdapter a = new SqlDataAdapter(command))
+                            {
+                            a.Fill(dt);
+                            }
+                        return dt;
+                        }
+                    catch (Exception ex)
+                        {
+                      
+                        Log.Fatal("Error running ExecuteReaderAsync.",ex.Message);
+                        
+                        return dt;
+                        }
+                    finally
+                        {
+                        connection.Close();
+                        }
+                    }
+                }
+            }
 
         //public async Task<object> ExecuteScalarAsync(CommandType cmdType,string cmdText,params SqlParameter[] commandParameters) {
         //    var result = new ServiceProcessingResult<object> { IsSuccessful = true };
