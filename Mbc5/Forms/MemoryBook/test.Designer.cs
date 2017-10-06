b825e172649f85b47cf1810ce44698d6b4916f11
rwoodall1 <randy@305spin.com>
2017-10-05 22:09:24 -0500

    commit-contactsave

--- Mbc5/Forms/MemoryBook/test.Designer.cs
+++ Mbc5/Forms/MemoryBook/test.Designer.cs
@@ -25,7 +25,6 @@
         private void InitializeComponent() {
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // printDialog1
@@ -36,22 +35,11 @@
             // 
             this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(237, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
             // test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 726);
-            this.Controls.Add(this.button1);
             this.Name = "test";
             this.Text = "test";
             this.ResumeLayout(false);
@@ -61,6 +49,5 @@
         #endregion
         private System.Windows.Forms.PrintDialog printDialog1;
         private System.Drawing.Printing.PrintDocument printDocument1;
-        private System.Windows.Forms.Button button1;
     }
     }
