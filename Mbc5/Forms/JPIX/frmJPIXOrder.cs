b1ea05403cacfac9d9ae7a71fa6b2a05387e6f59
Randy Woodall <randy.woodall@jostens.com>
2025-08-14 11:47:10 -0500

    Commit

--- Mbc5/Forms/JPIX/frmJPIXOrder.cs
+++ Mbc5/Forms/JPIX/frmJPIXOrder.cs
@@ -95,70 +95,82 @@ namespace Mbc5.Forms.JPIX
         {
             var processingResult = new ApiProcessingResult();
             var sqlClient = new SQLCustomClient().CommandText(@"Insert Into JPIXOrders
-           (Document
-           ,NeedsByDate
-            ,ProjectedShipDate
-           ,ProductType
-           ,Quantity
-           ,ShipToContact
-           ,ShipToCustomerName
-           ,ShipToAddress1
-           ,ShipToAddress2
-           ,ShipToCity
-           ,ShipToStateOrProvince
-           ,ShipToPostalCode
-           ,ShipToCountry
-           ,ShipToGroup
-           ,Reference
-           ,DateReceived
-           ,OracleCode,RequestId,OrderStatus) Values(
-            @Document
-           ,@NeedsByDate
-            ,@ProjectedShipDate
-           ,@ProductType
-           ,@Quantity
-           ,@ShipToContact
-           ,@ShipToCustomerName
-           ,@ShipToAddress1
-           ,@ShipToAddress2
-           ,@ShipToCity
-           ,@ShipToStateOrProvince
-           ,@ShipToPostalCode
-           ,@ShipToCountry
-           ,@ShipToGroup
-           ,@Reference
-           ,@DateReceived
-           ,@OracleCode,@RequestId,@OrderStatus)");
+           (
+                Document
+                ,NeedsByDate
+                ,ShipToCustomerName
+                ,ProjectedShipDate
+                ,ProductType
+                ,Quantity
+                ,ShipToContact
+                ,ShipToAddress1
+                ,ShipToAddress2
+                ,ShipToCity
+                ,ShipToStateOrProvince
+                ,ShipToPostalCode
+                ,ShipToCountry
+                ,ShipToGroup
+                ,Reference
+                ,DateReceived
+            ,OracleCode
+            ,RequestId
+            ,OrderStatus
+                ,EventCode)
 
+Values( 
+                @Document
+                ,@NeedsByDate
+                ,@ShipToCustomerName
+                ,@ProjectedShipDate
+                ,@ProductType
+                ,@Quantity
+                ,@ShipToContact
+                ,@ShipToAddress1
+                ,@ShipToAddress2
+                ,@ShipToCity
+                ,@ShipToStateOrProvince
+                ,@ShipToPostalCode
+                ,@ShipToCountry
+                ,@ShipToGroup
+                ,@Reference
+                ,@DateReceived
+                 ,@OracleCode
+                ,@RequestId
+                ,@OrderStatus
+                ,@EventCode
+           )");
 
             foreach (var order in jpixOrders.JostensPIXOrder)
             {
+
                 sqlClient.ClearParameters();
-                sqlClient.AddParameter("@Document", "\\\\sedsujpisl01\\workflow\\JPixFlyers\\Archive\\" + jpixOrders.RequestId + "\\" + order.JostensPIXOrderItem.Document);
-                sqlClient.AddParameter("@NeedsByDate", order.JostensPIXOrderItem.DateNeedsByDate);
-                sqlClient.AddParameter("@ProjectedShipDate", order.JostensPIXOrderItem.DateNeedsByDate.AddDays(-4));
-                sqlClient.AddParameter("@ProductType", order.JostensPIXOrderItem.ProductType);
-                sqlClient.AddParameter("@Quantity", order.JostensPIXOrderItem.Quantity);
-                sqlClient.AddParameter("@ShipToContact", order.JostensPIXOrderItem.ShipToContact);
-                sqlClient.AddParameter("@ShipToCustomerName", order.JostensPIXOrderItem.ShipToCustomerName);
-                sqlClient.AddParameter("@ShipToAddress1", order.JostensPIXOrderItem.ShipToAddress1);
-                sqlClient.AddParameter("@ShipToAddress2", order.JostensPIXOrderItem.ShipToAddress2);
-                sqlClient.AddParameter("@ShipToCity", order.JostensPIXOrderItem.ShipToCity);
-                sqlClient.AddParameter("@ShipToStateOrProvince", order.JostensPIXOrderItem.ShipToStateOrProvince);
-                sqlClient.AddParameter("@ShipToPostalCode", order.JostensPIXOrderItem.ShipToPostalCode);
-                sqlClient.AddParameter("@ShipToCountry", order.JostensPIXOrderItem.ShipToCountry);
-                sqlClient.AddParameter("@ShipToGroup", order.JostensPIXOrderItem.ShipToGroup);
-                sqlClient.AddParameter("@Reference", order.JostensPIXOrderItem.Reference);
+                sqlClient.AddParameter("@Document", "\\\\sedsujpisl01\\workflow\\JPixFlyers\\Archive\\" + jpixOrders.RequestId + "\\" + order.Document);
+                sqlClient.AddParameter("@NeedsByDate", order.DateNeedsByDate);
+                sqlClient.AddParameter("@ProjectedShipDate", order.DateNeedsByDate.AddDays(-4));
+                sqlClient.AddParameter("@ProductType", order.ProductType);
+                sqlClient.AddParameter("@Quantity", order.Quantity);
+                sqlClient.AddParameter("@ShipToContact", order.ShipToContact);
+                sqlClient.AddParameter("@ShipToCustomerName", order.ShipToCustomerName);
+                sqlClient.AddParameter("@ShipToAddress1", order.ShipToAddress1);
+                sqlClient.AddParameter("@ShipToAddress2", order.ShipToAddress2);
+                sqlClient.AddParameter("@ShipToCity", order.ShipToCity);
+                sqlClient.AddParameter("@ShipToStateOrProvince", order.ShipToStateOrProvince);
+                sqlClient.AddParameter("@ShipToPostalCode", order.ShipToPostalCode);
+                sqlClient.AddParameter("@ShipToCountry", order.ShipToCountry);
+                sqlClient.AddParameter("@ShipToGroup", order.ShipToGroup.ToString());
+                sqlClient.AddParameter("@Reference", order.Reference);
                 sqlClient.AddParameter("@DateReceived", DateTime.Now);
-                sqlClient.AddParameter("@OracleCode", order.JostensPIXOrderItem.SchoolCode);
+                sqlClient.AddParameter("@OracleCode", order.SchoolCode);
+                sqlClient.AddParameter("@EventCode", order.EventCode);
                 sqlClient.AddParameter("@OrderStatus", "Received");
                 string DateId = DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Year.ToString();
                 sqlClient.AddParameter("@RequestId", jpixOrders.RequestId + DateId);
                 var result = sqlClient.Insert();
                 if (result.IsError)
                 {
-                    Log.Error("Failed to insert record for JPXIX document:" + order.JostensPIXOrderItem.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "Name:" + order.JostensPIXOrderItem.ShipToCustomerName + "|" + result.Errors[0].DeveloperMessage);
-                    MbcMessageBox.Error("Failed to insert record for document:" + order.JostensPIXOrderItem.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "Name:" + order.JostensPIXOrderItem.ShipToCustomerName + "|" + result.Errors[0].DeveloperMessage);
+                    Log.Error("Failed to insert record for JPXIX document:" + order.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "Name:" + order.ShipToCustomerName + "|" + result.Errors[0].DeveloperMessage);
+                    MbcMessageBox.Error("Failed to insert record for document:" + order.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "Name:" + order.ShipToCustomerName + "|" + result.Errors[0].DeveloperMessage);
+                    continue;
                 }
                 await InsertProduction(order, result.Data);
 
@@ -166,39 +178,39 @@ namespace Mbc5.Forms.JPIX
             return processingResult;
         }
 
-        private async Task<ApiProcessingResult> InsertProduction(JostensPIXFulfillmentRequestsJostensPIXOrder order, string invno)
+        private async Task<ApiProcessingResult> InsertProduction(JostensPIXFulfillmentRequestsJostensPIXOrderItem order, string invno)
         {
             var processingResult = new ApiProcessingResult();
             var sqlClient = new SQLCustomClient().CommandText(@"Insert INTO Produtn (Company,Contryear,OracleCode,Schcode,Invno,Prodno,NoCopies,KitRecvd,Prshpdte)
                                                                  Values(@Company,@Contryear,@OracleCode,@Schcode,@Invno,@Prodno,@NoCopies,@KitRecvd,@Prshpdte)");
             sqlClient.ClearParameters();
             // is actually oracle code
-            sqlClient.AddParameter("@OracleCode", order.JostensPIXOrderItem.SchoolCode);
+            sqlClient.AddParameter("@OracleCode", order.SchoolCode);
             sqlClient.AddParameter("@Invno", invno);
             sqlClient.AddParameter("@Schcode", "01");
             sqlClient.AddParameter("@Contryear", ConfigurationManager.AppSettings["Contryear"]);
             sqlClient.AddParameter("@Company", "JPX");
             sqlClient.AddParameter("@Prodno", "J" + invno.ToString());
-            sqlClient.AddParameter("@NoCopies", order.JostensPIXOrderItem.Quantity);
+            sqlClient.AddParameter("@NoCopies", order.Quantity);
             sqlClient.AddParameter("@KitRecvd", DateTime.Now);
-            sqlClient.AddParameter("@Prshpdte", order.JostensPIXOrderItem.DateNeedsByDate);
+            sqlClient.AddParameter("@Prshpdte", order.DateNeedsByDate);
             var insertResult = sqlClient.Insert();
             if (insertResult.IsError)
             {
-                Log.Error("Failed to insert produtn record for JPIX document:" + order.JostensPIXOrderItem.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
-                MbcMessageBox.Error("Failed to insert produtn record for document:" + order.JostensPIXOrderItem.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
+                Log.Error("Failed to insert produtn record for JPIX document:" + order.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
+                MbcMessageBox.Error("Failed to insert produtn record for document:" + order.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
             }
             sqlClient.ClearParameters();
             sqlClient.CommandText(@"Insert INTO Wip (Schcode,OracleCode,Invno)
                                   Values(@Schcode,@OracleCode,@Invno)");
             sqlClient.AddParameter("@Schcode", "JPX");
-            sqlClient.AddParameter("@OracleCode", order.JostensPIXOrderItem.SchoolCode);
+            sqlClient.AddParameter("@OracleCode", order.SchoolCode);
             sqlClient.AddParameter("@Invno", invno);
             var wipResult = sqlClient.Insert();
             if (wipResult.IsError)
             {
-                Log.Error("Failed to insert wip record for JPXIX document:" + order.JostensPIXOrderItem.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
-                MbcMessageBox.Error("Failed to insert wip record for document:" + order.JostensPIXOrderItem.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
+                Log.Error("Failed to insert wip record for JPXIX document:" + order.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
+                MbcMessageBox.Error("Failed to insert wip record for document:" + order.Document + " Invno:" + invno.ToString() + "|" + insertResult.Errors[0].DeveloperMessage);
             }
             return processingResult;
         }
@@ -219,17 +231,17 @@ namespace Mbc5.Forms.JPIX
                 try
                 {
 
-                    string fileToCopy = dropPath + order.JostensPIXOrderItem.Document;
-                    string destinationDirectory = workpath + order.JostensPIXOrderItem.Document;
+                    string fileToCopy = dropPath + order.Document;
+                    string destinationDirectory = workpath + order.Document;
                     File.Copy(fileToCopy, destinationDirectory, true);
 
-                    File.Copy(fileToCopy, newFolder + "\\" + order.JostensPIXOrderItem.Document, true);
+                    File.Copy(fileToCopy, newFolder + "\\" + order.Document, true);
                     File.Delete(fileToCopy);
 
                 }
                 catch (Exception ex)
                 {
-                    MbcMessageBox.Error("Failed to copy file:" + order.JostensPIXOrderItem.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "|" + ex.Message);
+                    MbcMessageBox.Error("Failed to copy file:" + order.Document + " RequestId:" + jpixOrders.RequestId.ToString() + "|" + ex.Message);
                     Log.Error(ex.Message);
                 }
 
