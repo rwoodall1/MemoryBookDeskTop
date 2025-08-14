8f84c05447887a871b21eada33599fcf7a1382e5
Randy Woodall <randy.woodall@jostens.com>
2025-08-14 12:14:08 -0500

    commit-final

--- Mbc5/Properties/AssemblyInfo.cs
+++ Mbc5/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MBC5")]
-[assembly: AssemblyDescription("Customer Service/Production Tracking Program New PrdTracking")]
+[assembly: AssemblyDescription("Customer Service/Production Tracking Program New PrdTracking NewPrdtrk")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jostens MemoryBook Company")]
 [assembly: AssemblyProduct("Mbc5")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("25.8.12.3")]
-[assembly: AssemblyFileVersion("25.8.12.3")]
+[assembly: AssemblyVersion("25.8.14.1")]
+[assembly: AssemblyFileVersion("25.8.14.1")]
